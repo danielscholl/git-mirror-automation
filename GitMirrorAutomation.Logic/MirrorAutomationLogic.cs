93ccc3b2db53f1292641e4df799d04df017d3c9e
Marc Stan <marcstan@live.com>
2020-04-19 16:02:00 +0200

    cache responses

--- GitMirrorAutomation.Logic/MirrorAutomationLogic.cs
+++ GitMirrorAutomation.Logic/MirrorAutomationLogic.cs
@@ -1,8 +1,10 @@
 ﻿using GitMirrorAutomation.Logic.Mirrors;
+using GitMirrorAutomation.Logic.Models;
 using GitMirrorAutomation.Logic.Sources;
 using GitMirrorAutomation.Logic.Targets;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -41,6 +43,11 @@ namespace GitMirrorAutomation.Logic
                 return;
             }
             _log.LogInformation($"{repos.Length - toMirror.Length} repositories are already mirrored, setting up mirrors for the remaining {toMirror.Length} repositories..");
+            var cachedResponse = new Dictionary<IRepositoryTarget, IRepository[]>();
+            foreach (var target in targets)
+            {
+                cachedResponse[target] = await target.GetRepositoriesAsync(cancellationToken);
+            }
             foreach (var repo in toMirror)
             {
                 _log.LogInformation($"Creating mirror for repository {repo.Name}");
@@ -48,7 +55,7 @@ namespace GitMirrorAutomation.Logic
                 // creating repositories is also idempotent
                 foreach (var target in targets)
                 {
-                    var existing = await target.GetRepositoriesAsync(cancellationToken);
+                    var existing = cachedResponse[target];
                     if (existing.Any(e => e.Name == repo.Name))
                         continue;
 
